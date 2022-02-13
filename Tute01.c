1ceac0579daa9c6df01c595d7b1f33c0b8c6af4c
Dinindu Konghawatte <99582038+IT21247972@users.noreply.github.com>
2022-02-13 08:52:14 +0000

    Completed Tutorials

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+
+ float mark1,mark2,average;
+
+ printf("Enter marks for subject 1 : ");
+ scanf("%f", &mark1);
+ printf("\nEnter marks for subject 2 : ");
+ scanf("%f", &mark2);
+
+ average = ((mark1+mark2)/2.0);
+
+ printf("\nAverage of marks : %.1f", average);
+
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,18 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int n;
+  float sum;
+
+  printf("Enter number : ");
+  scanf("%d",&n);
+
+  for (int i = 1;i<=n;i++)
+  {
+    sum = sum+i;    
+   
+  }
+   printf("Sum = %f", sum);
   return 0;
 }
 
